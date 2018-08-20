09f4ab87ff97db9752defb6bacd40eaa9e20a437
Jacqueline Buros Novik <jackinovik@gmail.com>
2018-08-20 12:52:10 -0400

    rename 8schools for c++ compat

--- test.R
+++ test.R
@@ -2,7 +2,7 @@ source('stan_helpers.function.R')
 schools_dat <- list(J = 8, 
                     y = c(28,  8, -3,  7, -1,  1, 18, 12),
                     sigma = c(15, 10, 16, 11,  9, 11, 10, 18))
-fit <- fit_stan_model(file = "stan/8schools.stan",
+fit <- fit_stan_model(file = "stan/eight_schools.stan",
                       data = schools_dat,
                       iter = 10, chains = 1)
 
